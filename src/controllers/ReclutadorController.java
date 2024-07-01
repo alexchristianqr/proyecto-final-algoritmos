d14df96d7a7e0d8aa7d046ff306e4c27e9f3416a
GustavoAIG <gustavoa_it@hotmail.com>
2024-06-30 22:08:03 -0500

    funcionalidad de la tabla empleo

--- src/controllers/ReclutadorController.java
+++ src/controllers/ReclutadorController.java
@@ -2,14 +2,19 @@ package controllers;
 
 import models.Reclutador;
 import core.services.ResponseService;
+import java.util.List;
+import models.Empleo;
+import services.EmpleoService;
 import services.ReclutadorService;
 
 public class ReclutadorController extends BaseController<Reclutador, ReclutadorService> {
 
     private final ReclutadorService reclutadorService;
+    private final EmpleoService empleoService;
 
     public ReclutadorController() {
         reclutadorService = new ReclutadorService();
+        empleoService = new EmpleoService();
     }
 
     public ResponseService<Boolean> registrarReclutador(Reclutador reclutador) {
@@ -18,6 +23,31 @@ public class ReclutadorController extends BaseController<Reclutador, ReclutadorS
 
         response.setSuccess(success);
         response.setMessage("reclutador registrado de manera exitosa");
+
+        return response;
+    }
+    
+    public ResponseService<Boolean> registrarEmpleo(Empleo empleo) {
+        ResponseService<Boolean> response = new ResponseService<>();
+
+        boolean success = empleoService.crearEmpleo(empleo);
+
+        response.setSuccess(success);
+        response.setMessage("empleo registrado correctamente");
+        response.setResult(null);
+
+        return response;
+    }
+    
+    public ResponseService<List<Object[]>> listarEmpleo(Empleo empleo) {
+        ResponseService<List<Object[]>> response = new ResponseService<>();
+
+        String[] columnNames = {"Titulo", "Empresa", "Sueldo", "Modalidad", "Estado"};
+        List<Object[]> resultado = empleoService.listarEmpleosPorReclutador(columnNames);
+
+        response.setSuccess(true);
+        response.setMessage("listado de empleos");
+        response.setResult(resultado);
 
         return response;
     }
