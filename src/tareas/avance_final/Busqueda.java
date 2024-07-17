7d95fa3195ebb368aa0d42f64e7c4dfa48ba5cc5
Alex Christian <aqr@limatours.com.pe>
2024-07-17 15:09:49 -0500

    actualizado

--- src/tareas/avance_final/Busqueda.java
+++ src/tareas/avance_final/Busqueda.java
@@ -1,10 +1,13 @@
 package tareas.avance_final;
 
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Queue;
+import java.util.Set;
 
 class Busqueda {
 
