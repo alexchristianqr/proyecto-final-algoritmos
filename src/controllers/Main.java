03a47da5e54e8b0f7bdfc666904887b949913b73
Alex Christian <aqr@limatours.com.pe>
2024-07-17 17:58:16 -0500

    main

--- src/controllers/Main.java
+++ src/controllers/Main.java
@@ -30,7 +30,7 @@ public class Main {
 
     // Ejecutar programa, mostrando la vista de Login
     public static void main(String[] args) {
-        //testViewLogin();
+        testViewLogin();
 
         /* AUTH */
         //testLogin("maria.gonzales@utp.edu.pe", "reclutador2024");
@@ -66,7 +66,7 @@ public class Main {
         /* ARCHIVOS */
         //testUploadPDF();
         //testDownloadPDF();
-        testReporteExperiencias();
+        //testReporteExperiencias();
     }
 
     public static void testViewLogin() {
